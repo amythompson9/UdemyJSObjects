d8c8e38e2ed268299f463998a5f6b0622406412f
Amy Thompson <amythompson9@gmail.com>
2018-06-27 20:22:56 -0400

    Object Notes

--- UdemyJSObjects/JavaScript.js
+++ UdemyJSObjects/JavaScript.js
@@ -1,1 +1,18 @@
-﻿
+﻿var dog = {
+    name: "Rusty",
+    breed: "Mutt",
+    age: 3
+}
+
+//to access age
+//dog["age"]
+//or
+//dog.age
+
+dog.age = 4;
+
+//or
+dog.age += 1;
+
+dog["name"] = "Tater";
+
