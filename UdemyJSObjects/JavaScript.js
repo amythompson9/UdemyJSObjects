c333c0b8a0ee45f6c363566d17a7453f2cb8f0cf
Amy Thompson <amythompson9@gmail.com>
2018-07-05 19:43:57 -0400

    Objects Exercises

--- UdemyJSObjects/JavaScript.js
+++ UdemyJSObjects/JavaScript.js
@@ -1,18 +1,36 @@
-﻿var dog = {
-    name: "Rusty",
-    breed: "Mutt",
-    age: 3
-}
+﻿//var dog = {
+//    name: "Rusty",
+//    breed: "Mutt",
+//    age: 3
+//}
 
-//to access age
-//dog["age"]
-//or
-//dog.age
+////to access age
+////dog["age"]
+////or
+////dog.age
 
-dog.age = 4;
+//dog.age = 4;
 
-//or
-dog.age += 1;
+////or
+//dog.age += 1;
 
-dog["name"] = "Tater";
+//dog["name"] = "Tater";
+
+//arrays and objects
+
+//var posts = [
+//    {
+//        title: "Cats are mediocre",
+//        author: "Colt"
+//        comments: ["Awesome post", "terrible post"]
+//    },
+//    {
+//        title: "Cats are actully awesome",
+//        author: "Cat Luvr"
+//        comments: ["<3", "I hate you."]
+//    },
+//]
+
+//posts[0].title returns "Cats are mediorce"
+//posts[1].comments[1] returns "I hate you"
 
